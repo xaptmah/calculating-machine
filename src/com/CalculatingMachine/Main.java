d52dc52cdf5d14a889479d87bdb9dbfbf53f5ac5
xaptmah <xaptmah2010@gmail.com>
2020-10-26 21:36:41 +0200

    1.03 added window.

--- src/com/CalculatingMachine/Main.java
+++ src/com/CalculatingMachine/Main.java
@@ -1,8 +1,30 @@
 package com.CalculatingMachine;
 
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Place Calculating Machine");
-
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main();
+            }
+        });
     }
+
+    private JFrame window;
+    public Main(){
+        window = new JFrame("Calculating Machine");
+        window.setSize(280,340);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+    //CalculatingMachine calculatingMachine = new CalculatingMachine();
+
+
 }

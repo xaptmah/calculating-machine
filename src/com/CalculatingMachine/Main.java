fae2e051d704e9622d406cc515d1d230c04b6ce0
xaptmah <xaptmah2010@gmail.com>
2020-10-27 19:51:13 +0200

    1.04 added class WindowCM. moved the window to the WindowCM.

--- src/com/CalculatingMachine/Main.java
+++ src/com/CalculatingMachine/Main.java
@@ -1,7 +1,5 @@
 package com.CalculatingMachine;
 
-
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Main {
@@ -9,22 +7,9 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Main();
+                WindiowCM windiowCM = new WindiowCM();
+                windiowCM.show();
             }
         });
     }
-
-    private JFrame window;
-    public Main(){
-        window = new JFrame("Calculating Machine");
-        window.setSize(280,340);
-        window.setLocationRelativeTo(null);
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
-    }
-
-    //CalculatingMachine calculatingMachine = new CalculatingMachine();
-
-
 }
---
+++ src/com/CalculatingMachine/WindiowCM.java
@@ -0,0 +1,18 @@
+package com.CalculatingMachine;
+
+import javax.swing.JFrame;
+
+public class WindiowCM {
+    private JFrame window = new JFrame("Calculating Machine");
+    public WindiowCM(){
+
+        window.setSize(280,340);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    public void show(){
+        window.setVisible(true);
+    }
+}

6d8002d2f1d934c03c5a83e920350231720da852
xaptmah <xaptmah2010@gmail.com>
2020-10-29 01:02:11 +0200

    1.08
    successful experiment.

--- src/com/CalculatingMachine/Panel/OutPanel.java
+++ src/com/CalculatingMachine/Panel/OutPanel.java
@@ -1,14 +1,14 @@
 package com.CalculatingMachine.Panel;
 
 
-import javax.swing.JPanel;
+
 import javax.swing.JTextField;
 
-public class OutPanel extends JPanel {
+public class OutPanel extends Buttons {
     public OutPanel() {
         setLayout(null);
         JTextField output = new JTextField();
-        output.setBounds(10,10,200,50);
+        output.setBounds(10,10,205,50);
         output.setEditable(false);
         add(output);
     }
